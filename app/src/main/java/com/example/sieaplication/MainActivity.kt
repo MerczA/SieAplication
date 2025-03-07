c965b9e25ca7152bf22cde667fa60ac710025c53
mercz <22151197@aguascalientes.tecnm.mx>
2025-03-07 16:36:09 -0600

    FixProblemswith

--- app/src/main/java/com/example/sieaplication/MainActivity.kt
+++ app/src/main/java/com/example/sieaplication/MainActivity.kt
@@ -36,7 +36,7 @@ fun ComposeMultiScreenApp() { //La navegacion entre pantallas
 
 @Composable
 fun SetupNavGraph(navController: NavHostController) { //Es el que nos va mandar a la pantalla del menu que vamos a querer por medio de una ruta
-    NavHost(navController = navController, startDestination = "Main_Menu") {
+    NavHost(navController = navController, startDestination = "Main_menu") {
         composable("main_menu") { MainMenuScreen(navController) }
         composable("test_screen") { TestScreen(navController) }
 
