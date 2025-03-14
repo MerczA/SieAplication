e4b9b50c23ceeee8a78c29b38380fa04c64e49b0
GEZA GEOVANNI MERCZ ARELLANO <22151197@aguascalientes.tecnm.mx>
2025-03-14 13:46:39 -0600

    Add the Palet of Colors Indigo

--- app/src/main/java/com/example/sieaplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/sieaplication/ui/theme/Color.kt
@@ -9,3 +9,21 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Indigo50 = Color(0xFFE8EAF6)
+val I100 = Color(0xFFC5CAE9)
+val I200 = Color(0xFF9FA8DA)
+val I300 = Color(0xFF7986CB)
+val I400 = Color(0xFF5C6BC0)
+val I500 = Color(0xFF3F51B5)
+val I600 = Color(0xFF3949AB)
+val I700 = Color(0xFF303F9F)
+val I800 = Color(0xFF283593)
+val I900 = Color(0xFF1A237E)
+val A100 = Color(0xFF8C9EFF)
+val A200 = Color(0xFF536DFE)
+val A400 = Color(0xFF3D5AFE)
+val A700 = Color(0xFF304FFE)
+
+
+
