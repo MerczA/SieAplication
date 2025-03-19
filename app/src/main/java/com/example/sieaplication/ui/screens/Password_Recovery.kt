26f7d3801b1750a1510e61989af6eb5230272521
04-velasco-mariana <22151240@aguascalientes.tecnm.mx>
2025-03-18 19:27:45 -0600

    Commit for Screens NewPasswordScreen and PasswordRecoveryScreen

---
+++ app/src/main/java/com/example/sieaplication/ui/screens/Password_Recovery.kt
@@ -0,0 +1,89 @@
+package com.example.sieaplication.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import com.example.sieaplication.R
+
+
+@Preview(showBackground = true)
+@Composable
+fun PasswordRecoveryScreen(){
+    var controlNumber by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Image(
+            painter= painterResource(id= R.drawable.logotec),
+            contentDescription = "Logo del ITA",
+            modifier = Modifier
+                .size(320.dp)
+        )
+        Text(
+            text="Ingrese Datos de Recuperacion",
+            fontSize= 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF303F9F),
+            modifier = Modifier
+                .padding(top=16.dp)
+        )
+        Spacer (
+            modifier = Modifier
+                .height(20.dp)
+        )
+        OutlinedTextField(
+            value= controlNumber,
+            onValueChange = {controlNumber = it},
+            label = {Text("Número de Control")},
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        Spacer (
+            modifier = Modifier
+                .height(12.dp)
+        )
+        OutlinedTextField(
+            value= email,
+            onValueChange = {email = it},
+            label = {Text("Correo Electrónico")},
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        Spacer (
+            modifier = Modifier
+                .height(12.dp)
+        )
+        Button(
+            onClick = { //navega a la pantalla de confirmacion de recuperacion
+            },
+            colors= ButtonDefaults.buttonColors(containerColor = Color(0xFF303F9F)),
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = "Recuperar", color = Color.White)
+        }
+
+    }
+}
--- app/src/main/java/com/example/sieaplication/ui/screens/prueba_mariana.kt
+++ app/src/main/java/com/example/sieaplication/ui/screens/prueba_mariana.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
@@ -25,6 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,6 +38,7 @@ import com.example.sieaplication.R
 fun LoginScreen(){
     var controlNumber by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
 
     Column (
         modifier = Modifier
@@ -79,7 +82,8 @@ fun LoginScreen(){
         OutlinedTextField(
             value = password,
             onValueChange = {password = it},
-            label = {Text("Clave de Acceso")},
+            label = {Text("Contraseña")},
+            visualTransformation = PasswordVisualTransformation(),//tapa la contraseña
             singleLine = true,
             modifier = Modifier
                 .fillMaxWidth(0.8f)
@@ -101,8 +105,30 @@ fun LoginScreen(){
             modifier = Modifier
                 .height(10.dp)
         )
-        TextButton(onClick = { /*recuperar contraseña*/ }) {
-            Text("Recuperar Clave de Acceso", color = Color(0xFF3D5AFE))
+        TextButton(onClick = { showDialog=true }) {
+            Text("Recuperar Contraseña", color = Color(0xFF3D5AFE))
+        }
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                containerColor = Color(0xFFE8EAF6),
+                titleContentColor = Color(0xFF303F9F),
+                textContentColor = Color(0xFF3F51B5),
+                confirmButton = {
+                    Button(
+                        onClick = { showDialog = false },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF3D5AFE))
+                    ) {
+                        Text("OK", color = Color.White)
+                    }
+                },
+                title = { Text("Recuperación de la Contraseña") },
+                text = { Text("La contraseña será enviada al correo que tienes registrado.") }
+            )
         }
     }
 }
+
+
+
+
